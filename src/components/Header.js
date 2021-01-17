2102b8cee3451c0166d83e36f1f1cd1b50b649d1
syedslegend786 <74596807+syedslegend786@users.noreply.github.com>
2021-01-18 02:54:56 +0500

    Update Header.js
    write developer name in header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
  const Header = () => {
     return(
-        <h2>Expense Tracker</h2>
+        <h2>Expense Tracker By Syed Salleh M.Shah</h2>
     )
 }
 export default Header;
